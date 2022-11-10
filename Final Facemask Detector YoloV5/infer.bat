27b6e1b24e7b0b8b4458ed3e6c7f7bf2ea99fbd5
Xrez31 <santiagoaj2001@gmail.com>
2022-11-10 21:01:14 +0800

    Made changes to detect.py

---
+++ Final Facemask Detector YoloV5/infer.bat
@@ -0,0 +1,5 @@
+python detect.py --weights weights\ModelA0\weights\best.pt --source Inferencevid.mp4 --save-txt --save-conf --save-crop --name ModelA0
+python detect.py --weights weights\ModelA0\weights\best.pt --source Inferencevid.mp4 --save-txt --save-conf --save-crop --name ModelA00.30.6 --iou-thres 0.3 --conf-thres 0.6
+
+
+pause
